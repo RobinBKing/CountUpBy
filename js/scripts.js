361358c2e5af6884cd0c107a9e38855a1131833c
Lindsey Raffa and Rob King <lindsey.raffa@gmail.com>
2016-01-21 14:38:00 -0800

    create test for compare create passing specs

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,25 @@
-
-
-
+var numberToCheck = function(toNumber) {
+  return numberCheck(toNumber);
+};
+var numberByCheck = function(numberInput) {
+  return numberCheck(numberInput);
+};
+var numberCheck = function(byNumber) {
+  return !isNaN(byNumber);
+};
+var numberCompare = function(toNumber, byNumber) {
+  if(numberCheck(toNumber) && numberCheck(byNumber)){
+    return (toNumber >= byNumber);
+  }
+  else{
+    return false;
+  }
+};
+var numberDiv = function(toNumber, byNumber) {
+  if(numberCompare(toNumber, byNumber)){
+    return Math.round((toNumber / byNumber));
+  }
+};
 
 
 
--- spec/specs.js
+++ spec/specs.js
@@ -1,3 +1,24 @@
 describe('countTo', function() {
+   it("is true if countTo is a number", function(){
+     expect(numberToCheck(50)).to.equal(true);
+   });
+   it("is false if countTo is not a number", function(){
+     expect(numberToCheck("this")).to.equal(false);
+   });
+   it("is true if countBy is a number", function(){
+     expect(numberByCheck(7)).to.equal(true);
+   });
+   it("is true if countTo is greater than or equal to countBy number", function(){
+     expect(numberCompare(45, 8)).to.equal(true);
+   });
+   it("is false if countTo is less than or equal to countBy number", function(){
+     expect(numberCompare(8, 45)).to.equal(false);
+   });
+   it("is false if countTo and countBy are not numbers", function(){
+     expect(numberCompare("this", "that")).to.equal(false);
+   });
+   it("returns integer of countTo divided by countBy", function() {
+     expect(numberDiv(22, 5)).to.equal(4);
+   });
 
 });
